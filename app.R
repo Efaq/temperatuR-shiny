050900bea9c13f7b062c5706a23d5d1b1439cab4
Elisio Quintino <elife599@student.liu.se>
2021-09-27 13:41:08 +0200

    commit saving work

---
+++ app.R
@@ -0,0 +1,73 @@
+#'INPUTS
+#'
+#'actionButton
+#'submitButton
+#'checkboxInput
+#'checkboxGroupInput
+#'dateInput
+#'dateRangeInput
+#'fileInput
+#'numericInput
+#'passwordInput
+#'radioButtons
+#'selectInput
+#'sliderInput
+#'textInput
+#'
+#'OUTPUTS
+#'
+#'dataTableOutput
+#'htmlOutput
+#'imageOutput
+#'plotOutput
+#'tableOutput
+#'textOutput
+#'uiOutput
+#'verbatimTextOutput
+#'
+#'RENDERS
+#'
+#'renderDataTable
+#'renderImage
+#'renderPlot
+#'renderPrint
+#'renderTable
+#'renderText
+#'renderUI
+#'
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  sliderInput(
+    inputId = "num",
+    label = "Choose a number",
+    value = 25,
+    min = 1,
+    max = 100
+  ),
+  selectInput(
+    inputId = "selection",
+    label = "Make a selection",
+    choices = c("1", "2", "3")
+  ),
+  plotOutput(
+    outputId = "plot"
+    
+  )
+
+)
+
+server <- function(input, output) {
+  
+  output$plot = renderPlot(
+    {
+      title = "random normal variables"
+      hist(rnorm(input$num), main = title)
+    }
+  )
+  
+}
+
+shinyApp(ui = ui, server = server)
