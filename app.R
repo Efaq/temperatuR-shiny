3da28aa594cfa3a4cc6c85d359326c8497ea714d
Elisio Quintino <elife599@student.liu.se>
2021-09-28 16:51:44 +0200

    formatting

--- app.R
+++ app.R
@@ -6,11 +6,11 @@ agent = temperaturNuAgent()
 df_stations = agent$getInfoStations()
 sorted_kommun_list = sort(unique(df_stations$kommun))
 
-moving_average = function(input, pivot){
+moving_average = function(input, pivot) {
   l = length(input)
   ma = rep(NA, l)
-  for (ind in (pivot:l)){
-    ma[[ind]] = mean(input[(ind-pivot+1):ind])
+  for (ind in (pivot:l)) {
+    ma[[ind]] = mean(input[(ind - pivot + 1):ind])
   }
   return(ma)
 }
@@ -32,51 +32,56 @@ ui <- fluidPage(
       uiOutput("id_selection_renderer"),
       uiOutput("mov_avrg_pivot_renderer")
     ),
-    mainPanel(
-      plotOutput(outputId = "plot")
-    )
+    mainPanel(plotOutput(outputId = "plot"))
   ),
   "Behind the scenes: station ID's and kommun's are fetched at start time from the API,
   and temperatures are fetched on demand when a station is selected."
 )
 
 server <- function(input, output) {
-  output$id_selection_renderer = renderUI(
-    {
-      selectInput(
-        inputId = "id_selection",
-        label = "Station ID",
-        choices = sort(row.names(df_stations[df_stations$kommun == input$kommun_selection,]))
-      )
-    }
-  )
-  output$mov_avrg_pivot_renderer = renderUI(
-    {
-      sliderInput(
-        inputId = "mov_avrg_pivot",
-        label = "Moving average range",
-        min = 1,
-        max = 48,
-        value = 12
-      )
-    }
-  )
+  output$id_selection_renderer = renderUI({
+    selectInput(
+      inputId = "id_selection",
+      label = "Station ID",
+      choices = sort(row.names(df_stations[df_stations$kommun == input$kommun_selection, ]))
+    )
+  })
+  output$mov_avrg_pivot_renderer = renderUI({
+    sliderInput(
+      inputId = "mov_avrg_pivot",
+      label = "Moving average range",
+      min = 1,
+      max = 48,
+      value = 12
+    )
+  })
   output$plot = renderPlot({
     title = "How cold were Xmas and New Year in Sweden?"
     temp_data = agent$getTForStation(input$id_selection,
                                      "2020-12-23-23-50",
                                      "2021-01-02-00-00")
-    temp_data = temp_data[!is.na(temp_data$temperatur), ]
+    temp_data = temp_data[!is.na(temp_data$temperatur),]
     if (nrow(temp_data) > 0) {
-      par(mar=c(8, 4, 2, 2) + 0.1)
-      plot(temp_data$temperatur, pch=20, axes=FALSE, xlab="", ylab = "Temperature (°C)", main = title)
+      par(mar = c(8, 4, 2, 2) + 0.1)
+      plot(
+        temp_data$temperatur,
+        pch = 20,
+        axes = FALSE,
+        xlab = "",
+        ylab = "Temperature (°C)",
+        main = title
+      )
       lines(temp_data$temperatur)
-      conditions = endsWith(temp_data$datetime, "00:00") | endsWith(temp_data$datetime, "12:00")
-      axis(1, las=2,
-           at = which(conditions),
-           labels=temp_data$datetime[conditions])
+      conditions = endsWith(temp_data$datetime, "00:00") |
+        endsWith(temp_data$datetime, "12:00")
+      axis(
+        1,
+        las = 2,
+        at = which(conditions),
+        labels = temp_data$datetime[conditions]
+      )
       axis(2)
-      lines(moving_average(temp_data$temperatur, pivot=input$mov_avrg_pivot),
+      lines(moving_average(temp_data$temperatur, pivot = input$mov_avrg_pivot),
             col = "red")
       
     } else {
@@ -84,7 +89,6 @@ server <- function(input, output) {
     }
     
   })
-  
 }
 
 shinyApp(ui = ui, server = server)
